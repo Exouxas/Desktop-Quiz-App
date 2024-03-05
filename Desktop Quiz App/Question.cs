9485562db619ec24743f5069fd03834f6282c149
Exouxas <exouxas@gmail.com>
2024-03-05 11:54:21 +0100

    Add project files.

---
+++ Desktop Quiz App/Question.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desktop_Quiz_App
+{
+    /// <summary>
+    /// Contains paramaters for a specific questions
+    /// </summary>
+    public class Question
+    {
+        private string _query; // The question in... question
+        private string[] _options; // List of potential options
+        private int _correctAnswer; // Index of _options
+        private float _value; // Arbitrary number
+        private float _penalty; // In percent
+
+
+    }
+}
---
+++ Desktop Quiz App/Quiz.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desktop_Quiz_App
+{
+    /// <summary>
+    /// Contains sets of questions and evaluates score
+    /// </summary>
+    public class Quiz
+    {
+
+    }
+}
