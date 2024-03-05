9f8a6b7119f6271e3e657492e22db01418fdf869
Exouxas <exouxas@gmail.com>
2024-03-06 00:11:46 +0100

    Restructure and add TODOs

--- Desktop Quiz App/MainWindow.xaml.cs
+++ Desktop Quiz App/MainWindow.xaml.cs
@@ -19,8 +19,18 @@ namespace Desktop_Quiz_App
         public MainWindow()
         {
             InitializeComponent();
-
-
         }
+
+        private void OpenButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Spawn a file dialog box
+        }
+
+        // TODO: Event handler for next question
+        // Either remove the previous question and add the new one, or update the control(s) to match
+
+        // Sample of how to add and set position of item in Grid:
+        // MainGrid.Children.Add(_welcomeScreen);
+        // Grid.SetRow(_welcomeScreen, 1);
     }
 }
