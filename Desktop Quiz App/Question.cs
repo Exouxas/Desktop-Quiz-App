609fb1a2f2b8040394a34e42e177dfc617ec48a7
Exouxas <exouxas@gmail.com>
2024-03-06 15:55:21 +0100

    Reorganize parameters

--- Desktop Quiz App/Question.cs
+++ Desktop Quiz App/Question.cs
@@ -10,17 +10,17 @@ namespace DesktopQuizApp
     /// Contains paramaters for a specific questions
     /// </summary>
     /// <param name="query">The question being asked</param>
-    /// <param name="options">An array of possible answers</param>
     /// <param name="correctAnswer">The index of the correct answer</param>
     /// <param name="value">An arbitrary value that the quiz can use to calculate score</param>
     /// <param name="penalty">The amount of negative points (out of the value) being subtracted upon a wrong answer</param>
-    public struct Question(string query, string[] options, int correctAnswer, float value, float penalty)
+    /// <param name="options">An array of possible answers</param>
+    public struct Question(string query, int correctAnswer, float value, float penalty, string[] options)
     {
         private string _query = query;
-        private string[] _options = options;
         private int _correctAnswer = correctAnswer;
         private float _value = value;
         private float _penalty = penalty;
+        private string[] _options = options;
 
         /// <summary>
         /// Checks whether the answer is correct
